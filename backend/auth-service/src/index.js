7fd657e5b7e87ec7cc2ebfdb1acd86250488299b
JeffryBerduo45 <jefryberduo45@gmail.com>
2026-03-15 21:20:02 -0600

    Configuración inicial de docker funcional

--- backend/auth-service/src/index.js
+++ backend/auth-service/src/index.js
@@ -13,6 +13,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
